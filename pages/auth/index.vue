ecaaf0567cb0828c08d950b0ef184224ebfa8a3d
HisashiHasebe <hhasebe@diverta.co.jp>
2023-01-05 10:18:26 +0800

    update:authページ追加

---
+++ pages/auth/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>認証が完了しました</p>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    async makeAccessToen() {
+      const payload = {
+        code: this.$route.query.code
+      }
+      await this.$axios.post('/rcms-api/1/instagram_auth', payload);
+    }
+  },
+  mounted($route) {
+    this.makeAccessToen();
+  }
+}
+</script>
