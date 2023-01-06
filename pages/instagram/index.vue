b6dd26439cb9335ec752393b34ac0d572f29ef4f
HisashiHasebe <hhasebe@diverta.co.jp>
2023-01-07 00:45:49 +0800

    update:インスタのページ追加

---
+++ pages/instagram/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h2>Instagram</h2>
+    <div class="square_contents">
+      <a v-for="(n,i) in response.details.media_url" :key=i href="https://www.instagram.com/" target="_blank">
+        <img :src=n>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $axios }) {
+    return {
+      response: await $axios.$get('/rcms-api/1/instagram/instagram'),
+    };
+  },
+};
+</script>
+
+<style>
+.square_contents {
+  display: flex;
+  flex-wrap: wrap;
+  width:720px;
+}
+.square_contents a {
+  display: block;
+  position: relative;
+  width: 31%;
+  margin: 1%;
+}
+.square_contents a::before {
+  content: "";
+  display: block;
+  padding-top: 100%;
+}
+.square_contents img {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  object-fit: cover;
+}
+</style>
